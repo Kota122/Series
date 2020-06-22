5e7a20ef0c94fefdeb5ecc8f451e5e565ef531f8
Kota122 <67023623+Kota122@users.noreply.github.com>
2020-06-21 23:26:36 -0400

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,11 @@
+<?php
+    $destino= "pixie.revenant@gmail.com";
+    $nombre= $_POST["Nombre"];
+    $correo= $_POST["Correo"];
+    $telefono= $_POST["Telefono"];
+    $mensaje= $_POST["Mensaje"];
+    $contenido= "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTelefono: " .  $telefono . "\nMensaje: " . $mensaje;
+    mail($destino,"Contacto", $contenido);
+    header("Location:index.html")
+
+?>
